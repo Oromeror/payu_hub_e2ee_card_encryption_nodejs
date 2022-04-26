12adea746c80f1493f1a69059610769c0e54a9fe
oromeror <oromeror@unbosque.edu.co>
2022-04-26 11:57:39 -0500

    jose JWE card encryption implementation

--- source/routes/controllers/card.ts
+++ source/routes/controllers/card.ts
@@ -13,7 +13,7 @@ import * as jose from 'jose'
 async function generateEncryptedCard(req: Request, res: Response, next: NextFunction) {
 
     //Body Params
-    let card: Card = { credit_card_number: req.params.card_number, cvv: req.params.credit_card_cvv }
+    let card: Card = { credit_card_number: req.body.credit_card_number, cvv: req.body.cvv }
 
     /*
      * Retrieve a PaymentsOs Key by Version
@@ -22,12 +22,10 @@ async function generateEncryptedCard(req: Request, res: Response, next: NextFunc
 
     try {
 
-        //Query Params
-        let account_id: string = req.params.account_id;
-        let key_name: string = req.params.key_name;
-        let key_version: string = req.params.key_version;
+        //Endpoint
+        const url = `https://api.paymentsos.com/accounts/${req.params.account_id}/keys/${req.params.key_name}/versions/${req.params.key_version}`;
 
-        // Config
+        //Config
         const config = {
             headers: {
                 'api-version': req.header('api-version')!.toString(),
@@ -35,20 +33,45 @@ async function generateEncryptedCard(req: Request, res: Response, next: NextFunc
                 'Authorization': req.headers.authorization!.toString()
             }
         };
-        //Endpoint
-        const url = `https://api.paymentsos.com/accounts/${account_id}/keys/${key_name}/versions/${key_version}`;
 
         //Request
         const result: AxiosResponse = await axios.get(url, config);
-        
+
         if (result.status === 200) {
+            const key: Key = result.data;
             
-            let key: Key = result.data;
-            const thumbprint = await jose.calculateJwkThumbprint(key.jwk)
+            const rsaPublicKey = await jose.importJWK({
+                e: key.jwk.e,
+                n: key.jwk.n,
+                kty: key.jwk.kty,
+                kid: key.jwk.kid,
+                use: key.jwk.use,
+                alg: 'RS256'
+                })
+            
+            const TOKEN_TTL_MIN = 10; // Used to compute the expiration date that will be added to protected_headers 
+            const CREDIT_CARD_DATA = JSON.stringify(card);
+            const createdDate = new Date();  // Creating the date object so we can add it to protected_headers
+            const expiredDate = new Date(createdDate);
+           
+            expiredDate.setMinutes(expiredDate.getMinutes() + TOKEN_TTL_MIN);
+
+            const jwe = await new jose.GeneralEncrypt( new TextEncoder().encode(CREDIT_CARD_DATA))
+                .setProtectedHeader({
+                    jwk: key.jwk,
+                    enc: key.protected_headers.enc,
+                    kid: key.protected_headers.kid,
+                    iat: createdDate.getTime(),
+                    exp: expiredDate.getTime(),
+                    alg: 'RSA-OAEP-384'
+                })
+                .addRecipient(rsaPublicKey)
+                .encrypt()
 
             return res.status(200).json({
-                ciphertext: thumbprint
+                ciphertext: jwe
             });
+
         } else {
             return res.json({
                 message: result.data
@@ -56,7 +79,7 @@ async function generateEncryptedCard(req: Request, res: Response, next: NextFunc
         }
 
     } catch (err) {
-        console.error(err)
+        next(err)
         res.status(400).json({
             message: err
         })
